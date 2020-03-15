e049aa43dce81c9aeea9812e2431b3c39705c189
rhymermj <rhymermj@gmail.com>
2020-03-15 18:08:40 +1300

    Add superslides and typed js

--- js/script.js
+++ js/script.js
@@ -4,4 +4,12 @@ $(document).ready(function(){
         play: 5000,
 //        pagination: false
     });
+
+    var typed = new Typed(".typed", {
+        strings: ["Medical Laboratory Scientist", "IT Student"], 
+        typeSpeed: 70,
+        loop: true,
+        startDelay: 1000
+    })
+
 });
